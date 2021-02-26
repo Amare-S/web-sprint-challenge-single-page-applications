f895cf5ea4bf65e51ceb44562d119953d805a658
Amare Shakur <amare-shakur@lambdastudents.com>
2021-02-26 18:52:37 -0500

    lambda eats

--- src/App.js
+++ src/App.js
@@ -4,8 +4,35 @@ const App = () => {
   return (
     <>
       <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
     </>
+    
+    <div className="App">
+    <div className= "App">
+      <form>
+        <label>User
+         <input type="text" />
+        </label>
+
+        <label>Red
+       <input type="radio" />
+        </label>
+
+        <label>Blue 
+          <input type="radio"/>
+        </label>
+        <label>
+          <input type="checkbox"/>
+        </label>
+        <label> Language
+          <select>
+           <option>JavaScript</option>
+           <option>Python</option>
+           <option>Java</option>
+          </select>
+        </label>
+      </form>
+    </div>
+   </div>
   );
 };
 export default App;
